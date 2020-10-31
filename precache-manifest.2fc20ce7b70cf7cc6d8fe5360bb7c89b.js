486c30d3e01f60214e0ab892c0fa9a05a4494c7e
diogoku <diogomnribeiro@gmail.com>
2020-10-31 18:19:35 +0000

    Updates

---
+++ precache-manifest.2fc20ce7b70cf7cc6d8fe5360bb7c89b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3046927c8d9a63a6a94fec0f00f0c17",
+    "url": "/beats-frontend-store/index.html"
+  },
+  {
+    "revision": "c7a450b471feaa6107a7",
+    "url": "/beats-frontend-store/static/css/0.9879d526.chunk.css"
+  },
+  {
+    "revision": "fbaa4454ff2a4d44b45b",
+    "url": "/beats-frontend-store/static/css/3.4707e12a.chunk.css"
+  },
+  {
+    "revision": "950caef778935ef190c8",
+    "url": "/beats-frontend-store/static/css/main.c3ca22e4.chunk.css"
+  },
+  {
+    "revision": "c7a450b471feaa6107a7",
+    "url": "/beats-frontend-store/static/js/0.e537abff.chunk.js"
+  },
+  {
+    "revision": "fbaa4454ff2a4d44b45b",
+    "url": "/beats-frontend-store/static/js/3.48d562a7.chunk.js"
+  },
+  {
+    "revision": "51c98ade4fc82ab50e843e53cd615630",
+    "url": "/beats-frontend-store/static/js/3.48d562a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "950caef778935ef190c8",
+    "url": "/beats-frontend-store/static/js/main.840afd5e.chunk.js"
+  },
+  {
+    "revision": "5b2e335a507d0268b446",
+    "url": "/beats-frontend-store/static/js/runtime-main.30147fa2.js"
+  }
+]);
